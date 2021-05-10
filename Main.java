609041e9769ca85e825ff87c03f8d95759ee8eb9
VH Praneeth <vhpraneeth2001@gmail.com>
2021-05-10 10:52:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,49 @@ import java.io.*;
 
 class Main	{
 	public static void main(String[] args)	{
+		Scanner sc = new Scanner(System.in);
 		
+		// Create nodes
+		Node mumbai = new Node("India_Mumbai");
+		Node chennai = new Node("India_Chennai");
+		
+		HashMap<String, Node> hm = new HashMap();
+		hm.add("India_Mumbai", mumbai);
+		hm.add("India_Chennai", chennai);
+
+		Node srcNode, dstnNode;
+		String[] arr;
+		String line;
+
+		File myObj = new File("connections.txt");
+		Scanner myReader = new Scanner(myObj);
+		while (myReader.hasNextLine()) {
+			line = myReader.nextLine();
+			arr = line.split(' - ');
+			srcNode = hm.get(arr[0]);
+			dstnNode = hm.get(arr[1]);
+			srcNode.connect(dstnNode);
+		}
+		myReader.close();
+		
+		// Take user input
+		String src = "India_Mumbai";
+		String dstn = "Singapore";
+		
+		RouteFinder finder = new RouteFinder();
+		finder.find(src, dstn);  // prints best route
+	}
+}
+
+class Node	{
+	String name;
+	Node()	{ }
+	Node(String name)	{
+		this.name = name;
+	}
+	
+	ArrayList<Node> connections = new ArrayList();
+	void connect(Node dstn, int distance)	{
+		connections.add(dstn);
 	}
 }
