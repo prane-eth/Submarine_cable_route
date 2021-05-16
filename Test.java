09dd83dcd5e7ac8bf4daa50c31776ba2e05e6fec
vhpraneeth <praneeth.18bce7147@vitap.ac.in>
2021-05-16 11:55:03 +0530

    Edited files

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Test	{
+	public static void main(String[] args)	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter source: ");
+		String src = sc.next();
+		System.out.print("Enter destination: ");
+		String dstn = sc.next();
+
+		System.out.println("Route: \n India_Mumbai -> India_Chennai -> Singapore -> USA");
+	}
+}
