dbb9b6879853bbd1b80fff4e0923f1ceafd339ab
vhpraneeth <praneeth.18bce7147@vitap.ac.in>
2021-05-20 14:14:57 +0530

    Edited files

--- RouteFinder.java
+++ RouteFinder.java
@@ -18,46 +18,49 @@ class RouteFinder	{
 	}
 	static boolean read_data()	{
 		try {
-			Scanner sc = new Scanner(new File("matrix.txt"));
+			File my_file = new File("matrix.txt");
+			Scanner sc = new Scanner(my_file);
 			int rows = 0;
 			int columns = 0;
-			while (sc.hasNextLine()) {
+			while (sc.hasNextLine()) { // count rots
 				rows++;
-				Scanner colReader = new Scanner(sc.nextLine());
-				while (colReader.hasNextInt())
+				Scanner col_reader = new Scanner(sc.nextLine());
+				while (col_reader.hasNextInt())  // count columns
 					columns++;
+				System.out.println(rows + " " + columns);
 			}
 			int[][] arr = new int[rows][columns];
 			sc.close();
 
 			// read in the data
-			sc = new Scanner(new File("array.txt"));
-			for (int i = 0; i < rows; i++)
-				for (int j = 0; j < columns; j++)
+			sc = new Scanner(my_file);
+			for (int row = 0; row < rows; row++)
+				for (int col = 0; col < columns; col++)
 					if (sc.hasNextInt())
-						arr[i][j] = sc.nextInt();
+						arr[row][col] = sc.nextInt();
+			sc.close();
+
 			return true;
-		} catch (FileNotFoundException e) {
-			System.out.println("Sorry. File not found");
+		} catch (Exception e) {
+			System.out.println("Sorry... matrix file not found");
 			return false;
 		}
 	}
 }
 
-
 class DijShortestPath {
 	int[][] graph;
-	int V;
+	int len;
 	HashMap<Integer, String> hm;
 
-	DijShortestPath(int[][] graph, HashMap hm) {
+	DijShortestPath(int[][] graph, HashMap<Integer, String> hm) {
 		this.graph = graph;
-		V = graph.length;
+		len = graph.length;
 		this.hm = hm;
 	}
-	int minDistance(int dist[], Boolean sptSet[]) {
+	int minDistance(int[] dist, boolean[] sptSet) {
 		int min = Integer.MAX_VALUE, min_index = -1;
-		for (int v = 0; v < V; v++)
+		for (int v = 0; v < len; v++)
 			if (sptSet[v] == false && dist[v] <= min) {
 				min = dist[v];
 				min_index = v;
@@ -65,24 +68,25 @@ class DijShortestPath {
 		return min_index;
 	}
 	void process() {
-		int dist[] = new int[V];
-		Boolean sptSet[] = new Boolean[V];
-		for (int i = 0; i < V; i++) {
+		int dist[] = new int[len];
+		boolean sptSet[] = new boolean[len];
+		for (int i = 0; i < len; i++) {
 			dist[i] = Integer.MAX_VALUE;
 			sptSet[i] = false;
 		}
 		dist[0] = 0;
-		for (int count = 0; count < V - 1; count++) {
+		for (int count = 0; count < len-1; count++) {
 			int u = minDistance(dist, sptSet);
 			sptSet[u] = true;
-			for (int v = 0; v < V; v++)
+			for (int v = 0; v < len; v++)
 				if (!sptSet[v] && graph[u][v] != 0
 						&& dist[u] != Integer.MAX_VALUE
 						&& dist[u] + graph[u][v] < dist[v])
 					dist[v] = dist[u] + graph[u][v];
 		}
 		System.out.println("City \t\t Vertex \t\t Distance from Source");
-		for (int i = 0; i < V; i++)
-			System.out.println(hm.get(i) + " \t\t " + i + " \t\t " + dist[i]);
+		for (int i = 0; i < len; i++)
+			System.out.println(hm.get(i) + " \t\t "
+				 + i + " \t\t " + dist[i]);
 	}
 }
